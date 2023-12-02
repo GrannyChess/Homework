3e71598c3f588ea38a4764bce3c27e6b8ac4ad81
GrannyChess <[grannychess@yandex.ru]>
2023-12-02 21:43:20 +0300

    Третья задача решена

---
+++ HW03/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[a];
+
+void mas(int a)
+{
+Random rand = new Random();
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = rand.NextDouble();
+Console.Write($"{randomArray[i]:F2} ");
+}
+
+}
+
+double diff(double[] randomArray)
+{
+double min = randomArray[0];
+double max = randomArray[0];
+int i = 1;
+while (i < randomArray.Length)
+{
+if (max<randomArray[i])
+max = randomArray[i];
+if (min>randomArray[i])
+min = randomArray[i];
+i ++;
+}
+return max-min;
+}
+
+mas(a);
+Console.Write($"\nРазница между Max и Min элементами массива: {diff(randomArray):F2}");
